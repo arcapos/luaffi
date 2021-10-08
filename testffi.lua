5b4d40df98505aea2d41544b994bb736f203ed76
Marc Balmer <marc.balmer@arcapos.swiss>
2021-10-08 10:03:44 +0200

    Fix sample code

--- testffi.lua
+++ testffi.lua
@@ -2,7 +2,7 @@ local ffi = require 'ffi'
 local unix = require 'unix'
 
 
-local libc = unix.dlopen('/usr/lib64/libc-2.28.so', 1)
+local libc = unix.dlopen('/usr/lib64/libc-2.28.so', 'lazy')
 local t = libc.puts
 local puts = ffi.prep_cif(t, 'int32', 'pointer')
 print('puts', puts('hello, world'))
@@ -13,7 +13,7 @@ local f, status = ffi.prep_cif(libc.getpid, 'int32')
 print('getpid', f())
 os.execute('ps')
 
-local libm = unix.dlopen('/usr/lib64/libm-2.28.so', 1)
+local libm = unix.dlopen('/usr/lib64/libm-2.28.so', 'lazy')
 local floor, status = ffi.prep_cif(libm.floor, 'double', '*double')
 
 print(status, 'floor', floor(3.5))
